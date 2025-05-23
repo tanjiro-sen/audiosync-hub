2c30ae8841bfd80ffc4884df60fdaa65b2644bcc
Tanjiro <arshanthk@gmail.com>
2025-05-23 17:05:20 +0530

    Delete audio_player.py

--- audio_player.py
+++
@@ -1,9 +0,0 @@
-# audio_player.py
-import simpleaudio as sa
-import time
-
-def play_audio_at(audio_path, play_at_timestamp):
-    wave_obj = sa.WaveObject.from_wave_file(audio_path)
-    while time.time() < play_at_timestamp:
-        pass  # Busy wait or use sleep for less CPU load
-    wave_obj.play()
